fddc3a89486e3e34b46417fb17e021b72672d7e8
SuperCudder <144271447+SuperCudder@users.noreply.github.com>
2023-11-29 14:57:21 -0500

    just the test file

--- src/RunningExample.java
+++ src/RunningExample.java
@@ -4,10 +4,19 @@ public class RunningExample {
 		int[] arr = { 823, 124, 999, 331, 723 };
 
 		RadixSortQueue rsq = new RadixSortQueue();
-		RadixSortRC rsrc = new RadixSortRC();
-		System.out.print(rsq.sort(arr));
-		//System.out.println(rsrc.sort(arr));
-		
+		//RadixSortRC rsrc = new RadixSortRC();
+
+		int intArr[] = rsq.sort(arr);
+		//int intArrRC[] = rsrc.sort(arr);
+		//System.out.println(intArr.toString());
+
+		// System.out.println(intArrRC);
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(intArr[i]);
+			//System.out.println(intArrRC[i]);
+		}
+
 	}
 
 }
