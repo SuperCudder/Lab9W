25fdc4e88d5042f7ed26d338d785ee56a4553444
SuperCudder <144271447+SuperCudder@users.noreply.github.com>
2023-11-29 14:47:41 -0500

    Final version i think

--- src/RunningExample.java
+++ src/RunningExample.java
@@ -4,9 +4,9 @@ public class RunningExample {
 		int[] arr = { 823, 124, 999, 331, 723 };
 
 		RadixSortQueue rsq = new RadixSortQueue();
-		
+		RadixSortRC rsrc = new RadixSortRC();
 		System.out.print(rsq.sort(arr));
-		
+		//System.out.println(rsrc.sort(arr));
 		
 	}
 
